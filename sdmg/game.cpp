d84399cda383f5551172728b5a34d6c5bd9cc237
Panickev <panickev@hotmail.com>
2014-09-23 14:21:04 +0200

    Alles opnieuw gedaan
    Kutding crashed nog steeds

--- sdmg/game.cpp
+++ sdmg/game.cpp
@@ -1,8 +1,61 @@
 #include <iostream>
 #include "sdl\include\SDL.h"
+#include "lib\box2d\Box2D.h"
 
 int main(int argc, char **argv)
 {
+
+	b2Vec2 gravity(0.0f, -10.0f);
+	b2World world(gravity);
+
+	b2BodyDef groundBodyDef;
+	groundBodyDef.position.Set(0.0f, -10.0f);
+
+
+	b2Body* groundBody = world.CreateBody(&groundBodyDef);
+
+	b2PolygonShape groundBox;
+	groundBox.SetAsBox(50.0f, 10.0f);
+
+	groundBody->CreateFixture(&groundBox, 0.0f);
+
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_dynamicBody;
+	bodyDef.position.Set(0.0f, 4.0f);
+	b2Body* body = world.CreateBody(&bodyDef);
+
+
+	b2PolygonShape dynamicBox;
+	dynamicBox.SetAsBox(1.0f, 1.0f);
+
+
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &dynamicBox;
+	fixtureDef.density = 1.0f;
+	fixtureDef.friction = 0.3f;
+
+
+	body->CreateFixture(&fixtureDef);
+
+
+
+	float32 timeStep = 1.0f / 60.0f;
+	int32 velocityIterations = 6;
+	int32 positionIterations = 2;
+
+	for (int32 i = 0; i < 60; ++i)
+	{
+		world.Step(timeStep, velocityIterations, positionIterations);
+		b2Vec2 position = body->GetPosition();
+		float32 angle = body->GetAngle();
+		printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+	}
+
+
+
+
+
+
 	std::cin.get();
 	return 0;
 }
