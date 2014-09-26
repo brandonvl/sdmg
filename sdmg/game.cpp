f29c3c2bb1f3e41c2de6d287db3b02f50463192e
Wouter van Outersterp <admin@local.host>
2014-09-26 12:30:54 +0200

    Test code

--- sdmg/game.cpp
+++ sdmg/game.cpp
@@ -1,13 +1,69 @@
 #include <iostream>
 #include "sdl\include\SDL.h"
 #include "Engine.h"
+#include <map>
+#include <vector>
 
 using namespace SDMG;
 
+class Action {
+public:
+	virtual void run() = 0;
+	virtual Action* create() = 0;
+};
+
+class JumpAction : public Action {
+public:
+	void run()  {
+		// do something
+		std::cout << "Jumping";
+	}
+	Action* create() {
+		return new JumpAction();
+	}
+};
+
+
+std::map<char, Action*> *actions;
+std::vector<Action*> *actionContainer;
+
+// adds key binding
+void setKeyBinding(char key, Action *actionClass) {
+	actions->insert(std::pair<char, Action*>(key, actionClass));
+}
+
+
+// thing to do when an key is pressed
+void createAction() {
+	// key = 'A'
+	Action *action = ((*actions)['A'])->create();
+	actionContainer->push_back(action);
+}
+
+// run actions
+void runActions() {
+	// run all actions
+	for (int i = 0; i < actionContainer->size(); i++){
+		(*actionContainer)[i]->run();
+	}
+}
+
+
 int main(int argc, char **argv)
 {
-	Engine* engine = new Engine();
+	/*
+	actions = new std::map<char, Action*>;
+	actionContainer = new std::vector<Action*>;
 
-	SDL_Delay(10000);
+	setKeyBinding('A', new JumpAction());
+
+	createAction();
+	runActions();
+	*/
+
+	SDL_Keycode code = SDLK_2;
+
+	std::cin.get();
 	return 0;
 }
+
