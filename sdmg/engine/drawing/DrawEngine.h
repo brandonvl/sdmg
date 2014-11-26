96eab38fc28b7b94d6374632342921725057222c
Wouter van Outersterp <admin@local.host>
2014-11-26 15:50:42 +0100

    Copy map string toegevoegd

--- sdmg/engine/drawing/DrawEngine.h
+++ sdmg/engine/drawing/DrawEngine.h
@@ -49,6 +49,7 @@ namespace sdmg {
 				void loadMap(MovableGameObject *gameObject, MovableGameObject::State state, std::string path, float sliceWidth, float sliceHeight, float renderWidth, float renderHeight);
 				void loadMap(MovableGameObject *gameObject, MovableGameObject::State state, std::string path, float sliceWidth, float sliceHeight, float renderWidth, float renderHeight, Surface::AnimationType animationType);
 				void copyMap(MovableGameObject *gameObject, MovableGameObject::State copyFrom, MovableGameObject::State copyTo);
+				void copyMap(std::string str, MovableGameObject *gameObject);
 				void unload(std::string key);
 				void unloadText(std::string key);
 				void unloadAll();
